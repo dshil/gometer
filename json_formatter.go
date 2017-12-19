1bbcc746ef61ddeb4385e51adce2ea27558b475e
Pavel Nemtsev <nemtsevp@gmail.com>
2017-12-19 12:51:10 +0300

    refactor formatters

---
+++ json_formatter.go
@@ -0,0 +1,28 @@
+package gometer
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type jsonFormatter struct {
+}
+
+func (f *jsonFormatter) Format(counters map[string]Counter) []byte {
+	var buf bytes.Buffer
+
+	buf.WriteRune('{')
+	first := true
+	for _, k := range sortedKeys(counters) {
+		if first {
+			first = false
+		} else {
+			buf.WriteRune(',')
+		}
+		fmt.Fprintf(&buf, `"%s":%v`, k, counters[k].Get())
+	}
+	buf.WriteRune('}')
+	return buf.Bytes()
+}
+
+var _ Formatter = (*jsonFormatter)(nil)
